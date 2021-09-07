d358912f5eb58884dc5464ec81850de00b3af75c
unknown <yeejiac@gmail.com>
2021-09-07 19:42:31 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# WSL command
+
+- Change version 
+```
+wsl --set-version <distribution name> <versionNumber>
+```
+
+- Check all wsl distro
+```
+wsl --list --verbose
+```
+
+- shutdown command
+```
+wsl -t <DISTRO-NAME>
+```
---
+++ wsl_init.bat
@@ -0,0 +1,8 @@
+@REM Execute the following command to enable the device wsl
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+
+@REM wsl distro install
+
+@REM https://www.microsoft.com/zh-tw/p/ubuntu-1804-lts/9n9tngvndl3q?rtc=1&activetab=pivot:overviewtab
+
+@REM after install and execution, set password and username
